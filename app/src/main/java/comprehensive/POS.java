0109aaa290a1325ce62b660e8fb3939a65cb66d0
shuazs <varughesejoshua50@gmail.com>
2025-11-29 15:58:55 -0700

    finished POS and helper

--- app/src/main/java/comprehensive/POS.java
+++ app/src/main/java/comprehensive/POS.java
@@ -1,5 +1,37 @@
 package comprehensive;
-
+/**
+ * THE POS as an ENUM
+ */
 public enum POS {
     NOUN, VERB, ADJ, ADV, PRON, PREP, CONJ, INTERJ;
+
+
+    //Helper method to turn a string into an POS
+    public static POS txtToPOS(String txt){
+        if(txt == null){
+            throw new IllegalArgumentException("Txt was null!");
+        }
+        txt = txt.toUpperCase();
+        switch (txt) {
+            case "NOUN":
+                return POS.NOUN;
+            case "VERB":
+                return POS.VERB;
+            case "ADJ":
+                return POS.ADJ;
+            case "ADV":
+                return POS.ADV;
+            case "PRON":
+                return POS.PRON;
+            case "PREP":
+                return POS.PREP;
+            case "CONJ":
+                return POS.CONJ;
+            case "INTERJ":
+                return POS.INTERJ;
+            default:
+                throw new IllegalArgumentException("The POS you have is not valid!");
+        }
+
+    }
 }
