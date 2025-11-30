14e0160142cc94e523d88f7038864530cadef031
shuazs <varughesejoshua50@gmail.com>
2025-11-29 17:34:40 -0700

    main started and break time

---
+++ app/src/main/java/comprehensive/Main.java
@@ -0,0 +1,35 @@
+package comprehensive;
+
+public class Main {
+    public static void main(String[] args) {
+        Glossary glossary = new Glossary(args[0]);
+
+    }
+
+    // helper to get and read user input
+    private static String readLine() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String line = scanner.nextLine();
+        scanner.close();
+        return line;
+
+    }
+
+    //menu
+    private static void printMenu() {
+        System.out.println("Main menu");
+        System.out.println("1.  Get metadata");
+        System.out.println("2.  Get words in range");
+        System.out.println("3.  Get word");
+        System.out.println("4.  Get first word");
+        System.out.println("5.  Get last word");
+        System.out.println("6.  Get parts of speech");
+        System.out.println("7.  Update definition");
+        System.out.println("8.  Delete definition");
+        System.out.println("9.  Add new definition");
+        System.out.println("10. Save dictionary");
+        System.out.println("11. Quit");
+        System.out.println(" ");
+        System.out.print("Select an option: "); // colon + space
+    }
+}
